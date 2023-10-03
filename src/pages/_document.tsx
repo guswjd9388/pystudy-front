91819184614c687bcbfa809eceb8b13694c3d07e
김 현정 <hyunjeong2816@gmail.com>
2023-10-04 04:30:05 +0900

    test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-      <script src="https://t1.kakaocdn.net/kakao_js_sdk/2.4.0/kakao.min.js"></script>
+      <script defer src="https://t1.kakaocdn.net/kakao_js_sdk/2.4.0/kakao.min.js"></script>
       </Head>
       <body style={{ backgroundColor: 'rgb(243, 246, 249)' }}>
         <Main />
-        <NextScript />
+        <NextScript /> 
       </body>
     </Html>
   )
